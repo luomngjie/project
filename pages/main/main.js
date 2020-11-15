66e25c2d0fc0a8594100cac6e845bb904819ef4a
heku <990972975@qq.com>
2020-11-15 17:49:37 +0800

    新增个人中心页面

--- pages/main/main.js
+++ pages/main/main.js
@@ -5,6 +5,20 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
+    list:[
+      {
+        name:"使用教程",
+        id:0
+      },
+      {
+        name:"联系客服",
+        id:1
+      },
+      {
+        name:"赞赏作者",
+        id:2
+      }
+    ],
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -17,6 +31,7 @@ Page({
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
+     
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
@@ -24,7 +39,7 @@ Page({
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+      app.userInfoReadyCallback = res => {   
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
@@ -42,6 +57,7 @@ Page({
         }
       })
     }
+   
   },
   getUserInfo: function(e) {
     console.log(e)
