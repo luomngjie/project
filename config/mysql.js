f13ea7cbe4414060e63585ee7027c0e932eb9d77
heku <990972975@qq.com>
2020-11-20 14:21:17 +0800

    1

--- config/mysql.js
+++ config/mysql.js
@@ -3,15 +3,15 @@
  * 数据库相关信息
  */
 mysql = {
-    host:"",           //这是数据库的地址
+    host:"bdm266102296.my3w.com",           //这是数据库的地址
 	
-	user:"",                //需要用户名称  本地：root
+	user:"bdm266102296",                //需要用户名称  本地：root
 	
-	password:"",    //用户密码 ，如果你没有密码，直接双引号就是
+	password:"LMJANDly9401260",    //用户密码 ，如果你没有密码，直接双引号就是
 	
 	port:'3306',                    //端口
 	
-	database:""             //数据库名字
+	database:"bdm266102296_db"             //数据库名字
 }
 
 module.exports = mysql
